17f9518339116b7d57fb7af137726fd676bb1b39
amrut <amrut@DESKTOP-1VSDUBV>
2023-03-09 12:41:11 +0530

    3rd commit- wp1

--- src/test/java/practice/FlipkartLaptop.java
+++ src/test/java/practice/FlipkartLaptop.java
@@ -22,10 +22,10 @@ public class FlipkartLaptop {
 		driver.findElement(By.name("q")).sendKeys("laptop");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		List<WebElement> price = driver.findElements(By.xpath("//div[@class='_30jeq3 _1_WHN1'][not(number(translate(text(),'₹,',' '))>30000 and number(translate(text(),'₹,',' '))<50000)]"));
-		List<WebElement> laptopName = driver.findElements(By.xpath("//div[@class='_30jeq3 _1_WHN1'][not(number(translate(text(),'₹,',' '))>30000 and number(translate(text(),'₹,',' '))<50000)]/ancestor::div[@class='_3pLy-c row']/child::div/child::div[@class='_4rR01T']"));
+		List<WebElement> laptopNam = driver.findElements(By.xpath("//div[@class='_30jeq3 _1_WHN1'][not(number(translate(text(),'₹,',' '))>30000 and number(translate(text(),'₹,',' '))<50000)]/ancestor::div[@class='_3pLy-c row']/child::div/child::div[@class='_4rR01T']"));
 		for(WebElement p:price) {
 			
-			for(WebElement n:laptopName) {
+			for(WebElement n:laptopNam) {
 				System.out.println(n.getText()+"-->"+p.getText());
 				break;
 			}
